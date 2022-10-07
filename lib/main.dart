02368609243f2e8fb488ac02d985e71fa9fac557
Zain Naqvi <Zain.N@devnatives.com>
2022-10-07 22:55:20 +0500

    bloc using the cubit randam names in iterable

--- lib/main.dart
+++ lib/main.dart
@@ -30,28 +30,53 @@ class HelloWorld extends StatefulWidget {
 }
 
 class _HelloWorldState extends State<HelloWorld> {
-  late final Cubit cubit;
+  late final NamesCubits cubit;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    cubit = NamesCubits();
+  }
+
+  @override
+  void dispose() {
+    cubit.close();
+    // TODO: implement dispose
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            TextButton(
-              onPressed: () {},
-              child: const Text(
-                "Hi",
-                style: TextStyle(color: Colors.black, fontSize: 18),
-              ),
+      body: StreamBuilder<String?>(
+        stream: cubit.stream,
+        builder: (context, snapshot) {
+          final button = Center(
+            child: TextButton(
+              onPressed: () => cubit.pickRandomNumber(),
+              child: Text("Pick randam name"),
             ),
-          ],
-        ),
+          );
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return button;
+            case ConnectionState.waiting:
+              return button;
+            case ConnectionState.active:
+              return Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(snapshot.data ?? ''),
+                    button,
+                  ],
+                ),
+              );
+            case ConnectionState.done:
+              return SizedBox();
+          }
+        },
       ),
     );
   }
